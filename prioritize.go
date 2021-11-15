fe900f615a0d076bddc1374e44e1ecc5c5e90afb
TobiasPiontek <tobias_piontek@gmx.de>
2021-11-15 12:52:26 +0100

    initial commit

---
+++ prioritize.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	v1 "k8s.io/api/core/v1"
+	schedulerapi "k8s.io/kubernetes/pkg/scheduler/apis/extender/v1"
+)
+
+type Prioritize struct {
+	Name string
+	Func func(pod v1.Pod, nodes []v1.Node) (*schedulerapi.HostPriorityList, error)
+}
+
+func (p Prioritize) Handler(args schedulerapi.ExtenderArgs) (*schedulerapi.HostPriorityList, error) {
+	log.Print("info: ", args.Pod)
+	//fmt.Println(p.Func(*args.Pod, args.Nodes.Items))
+	return p.Func(*args.Pod, args.Nodes.Items)
+}
