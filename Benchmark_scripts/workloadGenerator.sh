263a8b7c4319ab687a8f6623ca13b3f008bbbe45
TobiasPiontek <tobias_piontek@gmx.de>
2021-12-13 21:02:46 +0100

    basic changes to workload generator

--- Benchmark_scripts/workloadGenerator.sh
+++ Benchmark_scripts/workloadGenerator.sh
@@ -1,9 +1,29 @@
-echo "executing workload generation"
-cat pod-template.yaml | sed 's/NAME_LABEL/testpod123/' | kubectl apply -f -
+#calculate the remaining time to queue the next log api call precisely
+duration=1200
+cpumili=10
+#function block
+function waitToNextMinute {
+   currentTime=$(date +%s)
+   seconds=$(($(date +%s)%60)) #get seconds of current time
+   timeToSleep=$((60-$seconds))
+   sleep $timeToSleep
+}
 
-echo "waiting 120 seconds"
-sleep 60
+#waitToNextMinute
 
-cat pod-template.yaml | sed 's/NAME_LABEL/testpod123/' | kubectl delete -f -
+for podindex in {1..100}
+do
+   podname="testpod$podindex"
+   echo $podname
+   echo "Deploy Pod: $i"
+   cat idlePod.yaml | sed "s/"NAME_LABEL"/${podname}/g" | sed "s/"TIMEOUT_DURATION"/${duration}/g" | sed "s/"CPU_MILICORES"/${cpumili}/g" |  kubectl apply -f -
+   echo $(cat idlePod.yaml | sed "s/"NAME_LABEL"/${podname}/g" | sed "s/"TIMEOUT_DURATION"/${duration}/g" | sed "s/"CPU_MILICORES"/${cpumili}/g") >> log.txt
+   sleep 10
+done
 
-echo "deleted pod"
+echo "waiting for completion"
+sleep 1200
+kubectl get pod --field-selector=status.phase==Succeeded #list completed pods
+kubectl delete pod --field-selector=status.phase==Succeeded #delte completed pods
+
+echo "deleted completed pods"
