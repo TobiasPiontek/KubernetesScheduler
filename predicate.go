72a0ae202c3196e8e7fad95787aa40daddf1acb9
TobiasPiontek <tobias_piontek@gmx.de>
2022-01-07 14:44:17 +0100

    added trivial load shifting approach, moved directories

--- predicate.go
+++ predicate.go
@@ -22,11 +22,26 @@ func (p Predicate) Handler(args schedulerapi.ExtenderArgs) *schedulerapi.Extende
 		result, err := p.Func(*pod, node)
 		//log.Print("Get Error: ",err.Error())
 		log.Print("Get Result (boolean): ", result)
-		log.Print("Get Node Name", pod.GetName())
-		log.Print("Get Namespace ", pod.GetNamespace())
+		log.Print("Get Node Name: ", pod.GetName())
+		log.Print("Get Namespace: ", pod.GetNamespace())
 		log.Print("Get Labels: ", pod.GetLabels())
-		log.Print("Get Timestamp", pod.GetCreationTimestamp())
-		log.Print("", time.Now())
+
+		log.Print("---------- Get timestamp of pod ----------")
+		log.Print("Get pod timestamp: ", pod.GetCreationTimestamp())
+		log.Print("Get pod unix timestamp: ", pod.GetCreationTimestamp().Unix())
+		log.Print("Get pod hour: ", pod.GetCreationTimestamp().Hour())
+		log.Print("Get pod minute: ", pod.GetCreationTimestamp().Minute())
+		log.Print("Get pod seconds: ", pod.GetCreationTimestamp().Second())
+
+		log.Print("---------- Get timestamp of scheduler instance ----------")
+		log.Print("scheduler timestamp: ", time.Now())
+		log.Print("scheduler unix timestamp: ", time.Now().Unix())
+		log.Print("get scheduler hour: ", time.Now().Hour())
+		log.Print("Get scheduler minute: ", time.Now().Minute())
+		log.Print("Get scheduler second: ", time.Now().Second())
+
+		//node.Status.
+
 		log.Print("Pod UUID: ", pod.GetUID())
 		var labels = pod.GetLabels()
 		var starttime = time.Now()
@@ -35,8 +50,9 @@ func (p Predicate) Handler(args schedulerapi.ExtenderArgs) *schedulerapi.Extende
 		if err != nil {
 			canNotSchedule[node.Name] = err.Error()
 		} else {
-			if result {
-				if labels["realtime"] == "not-critical" {
+			if result { //blocked before 11 or after 20  mind one hour offset
+				// < 12 means blocked before 13:00
+				if labels["realtime"] == "not-critical" && (time.Now().Hour() < 12 || time.Now().Hour() > 20) {
 					log.Print("can not schedule!")
 					canNotSchedule[node.Name] = err.Error()
 				} else {
