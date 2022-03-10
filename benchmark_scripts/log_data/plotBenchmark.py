6782391cb02f0b16e1be387a596099255e988e0d
Tobias <tobias_piontek@gmx.de>
2022-03-10 16:16:29 +0100

    enhanced evaluation script by a lot

--- benchmark_scripts/log_data/plotBenchmark.py
+++ benchmark_scripts/log_data/plotBenchmark.py
@@ -2,38 +2,87 @@ import matplotlib.pyplot as plt
 import csv
 
 
-xLabelCount=24
+xLabelCount = 12
 
 x = []
 y = []
-  
-with open('utilization-logs.csv','r') as csvfile:
+
+
+# Function to calculate power consumption
+# https://dl.acm.org/doi/pdf/10.1145/1273440.1250665 page 15 Estimating Server Power Usage
+def power_estimation(percentage):
+    idle_power_watt = 212
+    max_power_watt = 597
+    scaling_power = max_power_watt-idle_power_watt
+    return idle_power_watt + scaling_power*percentage
+
+
+i = 0
+with open('utilization-logs.csv', 'r') as csvfile:
     lines = csv.reader(csvfile, delimiter=',')
     for row in lines:
-        x.append(row[0])
+        i = i+1
+        x.append(row[0][:-3])
+        print(row[7])
+        # print("testingdex: " + str(i))
+        # print(float(row[8]))
         y.append(float(row[7]))
   
-plt.plot(x, y, color = 'b', linestyle = 'solid',label = "CPU reservation")
+plt.plot(x, y, color='b', linestyle='solid', label="CPU reservation")
 
-xtics = []
-xlabels = []
+x_tics = []
+x_labels = []
 
 for date in range(1, len(x), int(len(x)/xLabelCount)):
-    xtics.append(date)
-    xlabels.append(x[date])
+    x_tics.append(date)
+    x_labels.append(x[date])
 
 
 axes = plt
 
-plt.xticks(rotation = 45)
-plt.xticks(xtics,xlabels)
+plt.xticks(rotation=20)
+plt.xticks(x_tics, x_labels)
 
-plt.fill_between(x,y)
+plt.fill_between(x, y)
 plt.xlabel('Times')
 plt.ylabel('CPU Reservation in %')
-plt.title('Kubernetes Cluster cpu reservation', fontsize = 20)
+plt.title('Kubernetes Cluster cpu reservation', fontsize=20)
 plt.grid()
-plt.ylim([0,100])
-plt.xlim([0,len(y)-1])
+plt.ylim([0, 100])
+plt.xlim([0, len(y)-1])
 plt.legend()
+plt.show()
+
+
+# calculate graph for power consumption
+
+power_consumption_of_cluster = []
+for cluster_utilization_measured in y:
+    power_consumption_of_cluster.append(power_estimation(cluster_utilization_measured/100))
+
+plt.plot(x, power_consumption_of_cluster)
+plt.xticks(x_tics, x_labels)
+plt.xticks(rotation=45)
+plt.show()
+
+
+# Plot power model
+
+utilization_list = []
+power_consumption_list = []
+for i in range(0, 11):
+    utilization = i*0.1
+    utilization_list.append(utilization * 100)
+    power_consumption_list.append(power_estimation(utilization))
+
+
+plt.clf()
+# plt.plot(x, y, color='b', linestyle='solid', label="CPU reservation")
+plt.ylim(0, max(power_consumption_list))
+plt.xlim(0, max(utilization_list))
+plt.title('Utilization to power transition model', fontsize=20)
+plt.xlabel('cluster utilization in %')
+plt.ylabel('cluster power consumption in watt')
+
+plt.plot(utilization_list, power_consumption_list)
 plt.show()
