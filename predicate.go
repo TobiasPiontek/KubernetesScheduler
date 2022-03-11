7c2a65e40bac031a372d03a6b884dad85cc4b324
TobiasPiontek <tobias_piontek@gmx.de>
2022-03-11 11:34:07 +0100

    small changes

--- predicate.go
+++ predicate.go
@@ -188,12 +188,10 @@ func get_current_day_as_float() []float64 {
 	log.Print("weekday is: ", weekday)
 	lookupvalue := weekday + (week-1)*7
 
-	lookupvalue = 65
+	lookupvalue = 69
 	//convert the sub string array to a float array
 
 	log.Print("get index for lookup: ", lookupvalue, " , Excel row: ", lookupvalue+1)
-	
-	
 
 	converted := make([]float64, len(co2_data[lookupvalue]))
 	for index, element := range co2_data[lookupvalue] {
